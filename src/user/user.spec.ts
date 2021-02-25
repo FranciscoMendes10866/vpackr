e86ba3423bfd9071d679a1366ab992c1a196e795
FranciscoMendes10866 <51827441+FranciscoMendes10866@users.noreply.github.com>
2021-02-25 19:04:55 +0100

    Update user.spec.ts

--- src/user/user.spec.ts
+++ src/user/user.spec.ts
@@ -2,48 +2,13 @@ import request from 'supertest'
 
 import app from '../app'
 
-const randomBytes = (length: number): string => {
-  let len: number
-  if (!length) {
-    len = 8
-  } else {
-    len = length
-  }
-  const chars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  let str: string = ''
-  for (let i = 0; i < len; i++) {
-    str += chars.charAt(Math.floor(Math.random() * chars.length))
-  }
-  return str
-}
-
-const username = randomBytes(9)
-const email = `${randomBytes(12)}@gmail.com`
-const password = randomBytes(24)
-
 describe('User Tests', () => {
-  it('Sign up', async () => {
-    return await request(app).post('/api/users/sign_up')
-      .send({
-        username: username,
-        email: email,
-        password: password
-      })
-      .then((res) => {
-        expect(res.statusCode).toBe(201)
-        expect(res.type).toBe('text/plain')
-      })
-  })
-  it('Sign in', async () => {
-    return await request(app).post('/api/users/sign_in')
-      .send({
-        email: email,
-        password: password
-      })
+  it('Does it run', async () => {
+    return await request(app).get('/api/users')
       .then((res) => {
         expect(res.statusCode).toBe(200)
         expect(res.type).toBe('application/json')
-        expect(res.body.username).toBe(username)
+        expect(res.body.hw).toBe('User controller 🌈 🍩 🍟')
       })
   })
 })
